6269ec4a72c984904b2675cbd6a6f29ac7d5140e
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2019-11-22 11:20:56 +0200

    loading added

--- src/pages.js
+++ src/pages.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import first from './images/first.png';
 import second from './images/second.png';
 import third from './images/third.png';
 import fourth from './images/fourth.png';
 import fifth from './images/fifth.png';
+import loading from './images/loading.gif';
+
+import axios from 'axios';
 
 function getUrlParameter(name) {
   name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -16,30 +19,64 @@ function getUrlParameter(name) {
 }
 const First = () => {
   return (
-    <img
-      style={{
-        width: '100vw'
-      }}
-      src={first}
-      alt=""
-    />
+    <>
+      <img
+        style={{
+          width: '100vw'
+        }}
+        src={first}
+        alt=""
+      />
+    </>
+  );
+};
+const Loading = function({ show = false }) {
+  return (
+    show && (
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: '10',
+          top: '206px',
+          right: '43px',
+          backgroundColor: 'white',
+          width: '400px',
+          height: '66px',
+          textAlign: 'center'
+        }}>
+        <img
+          src={loading}
+          alt=""
+          style={{
+            height: '50px'
+          }}
+        />
+      </div>
+    )
   );
 };
 const Second = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <img
-      onClick={async () => {
-        const companyId = getUrlParameter('companyId');
-        const userId = getUrlParameter('userId');
-        const pipeline_id = await createBundle({ companyId, userId });
-        window.open(`https://app.pipedrive.dev/pipeline/${pipeline_id}`);
-      }}
-      style={{
-        width: '100vw'
-      }}
-      src={second}
-      alt=""
-    />
+    <>
+      <Loading show={loading} />
+      <img
+        onClick={async () => {
+          setLoading(true);
+          const companyId = getUrlParameter('companyId');
+          const userId = getUrlParameter('userId');
+          const pipeline_id = await createBundle({ companyId, userId });
+          setLoading(false);
+
+          window.open(`https://app.pipedrive.dev/pipeline/${pipeline_id}`);
+        }}
+        style={{
+          width: '100vw'
+        }}
+        src={second}
+        alt=""
+      />
+    </>
   );
 };
 const Third = () => {
@@ -78,9 +115,20 @@ const Fifth = () => {
 const createBundle = async function({ companyId, userId }) {
   let pipeline_id = '';
   try {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
     const url = `http://localhost:50372/api/v1/tiers/bundle/apply?companyId=${companyId}&userId=${userId}&api_token=17b85132b37645fa3bf7e293246fd96d67f92588&=`;
-    const ping = await fetch(url);
-    const resp = await ping.json();
+    const client = axios.create({
+      headers: {
+        'Accept': 'application/json',
+        'Cache-control': 'no-cache',
+        'Pragma': 'no-cache'
+      },
+      mode: 'cors',
+      withCredentials: true,
+      crossdomain: true
+    });
+    const ping = await client({ url, method: 'GET' });
+    const resp = await ping.data;
     pipeline_id = resp.data.pipeline_id;
   } catch (e) {}
 
